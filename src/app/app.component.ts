40bb6aea7415859b70c79b51cdba92e8bc960d3b
Thiago Mussolini <tmussolini@apoioprodesp.sp.gov.br>
2025-11-27 00:14:12 -0300

    Criado a tela de login e os componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './componentes/container/container.component';
+import { LogoComponent } from './componentes/logo/logo.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ContainerComponent,
+    LogoComponent,
+    LoginComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'QA_Friend';
