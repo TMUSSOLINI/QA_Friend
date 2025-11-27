5f6166c133a66c9a8051fd996ab1deeba87c3e18
Thiago Mussolini <tmussolini@apoioprodesp.sp.gov.br>
2025-11-27 00:46:40 -0300

    Feat: Criado a parte que garante que a tela de login não fique com scrool, adicionando a class no body do html

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
+  constructor(private renderer: Renderer2) {}
 
+  ngOnInit(): void {
+    this.renderer.addClass(document.body, 'login-page-style');
+  }
+
+  ngOnDestroy(): void {
+    this.renderer.removeClass(document.body, 'login-page-style');
+  }
 }
